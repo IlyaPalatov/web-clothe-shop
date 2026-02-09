f20eb7464bb5127de237d0bfd616d671d5af1b24
IlyaPalatov <121305312+IlyaPalatov@users.noreply.github.com>
2026-02-09 23:59:36 +0200

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/web-clothe-shop/", // ← Змінити на назву свого репозиторію
+  base: "/", // ← Змінив тут
   resolve: {
     dedupe: ["react", "react-dom"],
   },
