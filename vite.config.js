4009fcf92d10dcf23c37bbe30946a8a5980e59ae
IlyaPalatov <121305312+IlyaPalatov@users.noreply.github.com>
2026-02-10 00:07:36 +0200

    Move files to root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ← Змінив тут
+  base: "/web-clothe-shop/",
   resolve: {
     dedupe: ["react", "react-dom"],
   },
